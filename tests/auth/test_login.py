43293d100af41cf82a4df5b01dc77d953e87cac2
Michał Martyniak <michal.martyniak@linux.pl>
2018-06-07 19:07:42 +0200

    Add example tests for pytest (run as: python -m pytest)
    Ignore .DS_Store

---
+++ tests/auth/test_login.py
@@ -0,0 +1,9 @@
+from reserver_app.auth.views import UserLogin
+import pytest
+
+class TestAuth:
+    def test_cond_1(self):
+        assert True is True
+
+    def test_cond_2(self):
+        assert False is False
